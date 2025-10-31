0b8d2bc7554459a096d38a429757fe2c50cf74f8
Narisoa <andriamananandroh@gmail.com>
2025-10-30 23:45:32 -0900

    add fonction start

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
+"use strict";
 import "./style.css";
+const btnStart = document.querySelector(".start");
+const btnPause = document.querySelector(".pause");
+const btnReset = document.querySelector(".reset");
+const timer = document.querySelector(".timer");
+let interval = null;
+let minutes = 0;
+let seconds = 0;
+
+const timerInterval = () => {
+  interval = setInterval(() => {
+    seconds--;
+    if (seconds < 0) {
+      minutes--;
+      seconds = 8;
+    }
+    timer.textContent = `${minutes < 10 ? "0" + minutes : minutes}:${
+      seconds < 10 ? "0" + seconds : seconds
+    }`;
+    if (minutes === 0 && seconds === 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+};
+
+const startTimer = () => {
+  minutes = 25;
+  seconds = 0;
+  timerInterval();
+  btnStart.disabled = true;
+};
+btnStart.addEventListener("click", startTimer);
